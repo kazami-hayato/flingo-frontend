c70e697bd9de6bd49f2a1acaa1bcae4eb1b7146f
hayato <zerkvii@gmail.com>
2019-12-05 20:29:57 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -36,12 +36,24 @@ module.exports = {
       warnings: false,
       errors: true
     },
+    // proxy: {
+    //   // change xxx-api/login => mock/login
+    //   // detail: https://cli.vuejs.org/config/#devserver-proxy
+    //   [process.env.VUE_APP_BASE_API]: {
+    //     // target: `http://127.0.0.1:${port}/mock`,
+    //     target:'http://127.0.0.1:8080/',
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       ['^' + process.env.VUE_APP_BASE_API]: ''
+    //     }
+    //   }
+    // },
     proxy: {
       // change xxx-api/login => mock/login
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        // target: `http://127.0.0.1:${port}/mock`,
-        target:'http://127.0.0.1:5000/',
+        target: `http://127.0.0.1:${port}/mock`,
+        // target:'http://127.0.0.1:5000/',
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
