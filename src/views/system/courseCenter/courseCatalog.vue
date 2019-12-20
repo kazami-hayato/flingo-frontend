c4cdb1b592466dc5f2e6c9fe7f1d2d88ff9229fc
BooleanlN <1363371357@qq.com>
2019-12-20 12:37:26 +0800

    feat:目录上传

--- src/views/system/courseCenter/courseCatalog.vue
+++ src/views/system/courseCenter/courseCatalog.vue
@@ -33,22 +33,25 @@
             @click="() => remove(node, data)">
             删除该目录
           </el-button>
-            <span v-show="!data.isTitle">
-              <el-upload style="display: inline-block;" :auto-upload="false" ref="video"
-                         :file-list="fileList" :show-file-list="false" action="/dev-api/api/v1/shift/courses/video"
-                         accept="video/mp4"
-                         :on-change="addFile" :on-progress="progress" :on-success="finish" :on-error="handleError">
-              <el-button type="text" size="mini">选取视频</el-button>
-              <span style="font-size: 12px">(mp4文件,不超过{{maxSize}}mb)</span>
-            </el-upload>
-            <el-button type="text" size="mini" @click="uploadVideo(data)">上传</el-button>
-            <div style="width: 100px;display: inline-block;font-size: 11px;" v-show="data.uploading">
-              <el-progress :percentage="percentage" :text-inside="true" :stroke-width="12"/>
-            </div>
-            <div style="width: 100px;display: inline-block;font-size: 11px;" v-if="data.status">
-              已上传
-            </div>
-            </span>
+          <el-button type="text" size="large" @click="addVid(node,data)">
+          设置Vid
+          </el-button>
+            <!--<span v-show="!data.isTitle">-->
+              <!--<el-upload style="display: inline-block;" :auto-upload="false" ref="video"-->
+                         <!--:file-list="fileList" :show-file-list="false" action="/dev-api/api/v1/shift/courses/video"-->
+                         <!--accept="video/mp4"-->
+                         <!--:on-change="addFile" :on-progress="progress" :on-success="finish" :on-error="handleError">-->
+              <!--<el-button type="text" size="mini">选取视频</el-button>-->
+              <!--<span style="font-size: 12px">(mp4文件,不超过{{maxSize}}mb)</span>-->
+            <!--</el-upload>-->
+            <!--<el-button type="text" size="mini" @click="uploadVideo(data)">上传</el-button>-->
+            <!--<div style="width: 100px;display: inline-block;font-size: 11px;" v-show="data.uploading">-->
+              <!--<el-progress :percentage="percentage" :text-inside="true" :stroke-width="12"/>-->
+            <!--</div>-->
+            <!--<div style="width: 100px;display: inline-block;font-size: 11px;" v-if="data.status">-->
+              <!--已上传-->
+            <!--</div>-->
+            <!--</span>-->
         </span>
       </span>
           </el-tree>
@@ -63,19 +66,20 @@
 </template>
 <script>
   // import {uploadCourseCatalog,getCourseCatalog} from '../../api/school-course'
-  import {getCatalogTreeById} from '@/api/system_apis'
+  import {getCatalogTreeById,modifySystemCourseById} from '@/api/system_apis'
 
   export default {
     data() {
 
       return {
-        data: {},
+        data: [],
         fileList: [], // 上传文件列表
         maxSize: 500, // 上传文件限制大小，单位mb
         percentage: 0, // 上传进度
         tempData: {},
         course_id: undefined,
-        course: {}
+        course: {},
+        current_id:0
       }
     },
     created() {
@@ -90,17 +94,34 @@
       getCatalog() {
         getCatalogTreeById({course_id: this.course_id}).then(response => {
           this.course = response.data
+          console.log(JSON.parse(this.course.catalogtree))
           this.data = JSON.parse(this.course.catalogtree).catalogtree
+          this.getPlayList(this.data)
+         // console.log(this.current_id)
         }).catch(error => {
           console.log(error)
         })
+      },
+      /**
+       *  深度遍历获取id
+       * */
+      getPlayList(catalog){
+        for(let i=0;i<catalog.length;i++){
+          if(catalog[i].id > this.current_id){
+            this.current_id = catalog[i].id
+          }else if(catalog[i].hasOwnProperty('children')){
+            this.getPlayList(catalog[i]['children'])
+          }
+        }
       },
       /*
       * 保存新目录
       */
       updateCatalog() {
         console.log(this.data);
-        uploadCourseCatalog({course_id: this.course_id, catalog: this.data}).then(res => {
+        this.course.catalogtree = JSON.stringify({"catalogtree":this.data})
+        modifySystemCourseById(this.course).then(res => {
+          console.log(res)
           this.$notify({
             title: '成功',
             message: '更新成功',
@@ -119,7 +140,7 @@
       */
       append(data) {
         console.log(data)
-        const newChild = {id: ++id, label: '新目录', children: [], status: false}
+        const newChild = {id:  ++this.current_id, label: '新目录', children: [], status: false}
         if (!data.children) {
           this.$set(data, 'children', [])
         }
@@ -165,7 +186,7 @@
        * 添加新大章节
        */
       appendMain() {
-        const newChild = {id: ++id, label: '新目录', children: [], isTitle: true};
+        const newChild = {id: ++this.current_id, label: '新目录', children: [], isTitle: true};
         console.log(this.data)
         this.data.push(newChild)
       },
@@ -231,6 +252,26 @@
           title: '出错',
           message: '上传出错！',
           type: 'error'
+        });
+      },
+      addVid(node,data){
+        console.log(node,data)
+        this.$prompt('请输入该课程对应的Vid', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+        }).then(({ value }) => {
+          data.vid  = value
+          this.type = true
+            this.$message({
+            type: 'success',
+            message: '设置成功!'
+          });
+          console.log(this.data)
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消输入'
+          });
         });
       }
     }
